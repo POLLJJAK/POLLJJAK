92de0608d900042af8cf1308f8d383e5af20cefe
jp0301 <jp0301@naver.com>
2023-01-17 05:12:56 +0900

    Add : 세부업무 등록, 완료 추가

--- POLLJJAK/src/com/test/mybatis/IInnerProjectSubWorkDAO.java
+++ POLLJJAK/src/com/test/mybatis/IInnerProjectSubWorkDAO.java
@@ -6,10 +6,14 @@ import com.test.dto.InnerProjectSubWorkDTO;
 
 public interface IInnerProjectSubWorkDAO
 {
+	public String userName(String u_p_apply_code);
+	
 	public InnerProjectSubWorkDTO mainwork_title(String ph_mainwork_code);
 	public ArrayList<InnerProjectSubWorkDTO> mainwork_team(String ph_mainwork_code);
 	public int mainwork_percent(String ph_mainwork_code);
 	public ArrayList<InnerProjectSubWorkDTO>subwork_list(String ph_mainwork_code);
 	public ArrayList<InnerProjectSubWorkDTO>subwork_teamComment(String ph_mainwork_code);
 	
+	public int addSubwork(InnerProjectSubWorkDTO dto);
+	public int completeSubwork(String ph_subwork_code);
 }
